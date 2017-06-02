bf70b94d51198604abfa88430389742a065c6016
Rahaf97 <Rahaf.al.murebah@gmail.com>
2017-06-03 00:28:50 +0300

    Add files via upload

---
+++ Room.java
@@ -0,0 +1,24 @@
+   public class Room {
+      private String RoomNo;
+      private boolean available;
+      public Room (String no , boolean ava){
+         RoomNo=no;
+         available=ava;}
+   
+      public void setNum (String num){
+         RoomNo=num;}
+   
+      public void setNum (boolean v){
+         available=v;}
+   
+      public String getNum (){ 
+         return RoomNo;}
+			
+
+
+		public void setAvailable (boolean a){
+			available = false ;}
+      public boolean getAvailable (){ 
+         return available;}
+   
+   }
